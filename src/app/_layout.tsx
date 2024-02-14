c208bf493608ec867befa7e2edc88194f9bb910b
opatricpires <patric.pires@outlook.com>
2024-02-14 17:51:20 -0300

    start project

---
+++ src/app/_layout.tsx
@@ -0,0 +1,30 @@
+import {
+  Inter_400Regular,
+  Inter_500Medium,
+  Inter_600SemiBold,
+  Inter_700Bold,
+  useFonts,
+} from "@expo-google-fonts/inter";
+
+import { Loading } from "../components/loading";
+import { SafeAreaView } from "react-native";
+import { Slot } from "expo-router";
+
+export default function Layout() {
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_500Medium,
+    Inter_600SemiBold,
+    Inter_700Bold,
+  });
+
+  if (!fontsLoaded) {
+    return <Loading />;
+  }
+
+  return (
+    <SafeAreaView className="flex-1 bg-zinc-900">
+      <Slot />
+    </SafeAreaView>
+  );
+}
---
+++ src/app/index.tsx
@@ -0,0 +1,8 @@
+import { FlatList, SectionList, Text, View } from "react-native";
+import { useRef, useState } from "react";
+
+import { Link } from "expo-router";
+
+export default function App() {
+  return <View className="flex-1 pt-8"></View>;
+}
